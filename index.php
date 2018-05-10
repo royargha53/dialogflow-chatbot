c2935c999cbc1305b3c1b3cfd99618aac8a709cb
Phani-Gorantla <38903564+Phani-Gorantla@users.noreply.github.com>
2018-05-10 18:29:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
+$speech = "Ok";
 	$text = $json->result->parameters->text;
 
 	switch ($text) {
